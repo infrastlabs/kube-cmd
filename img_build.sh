5d08f85e79e0c96eb8b83ae9592c7ce959b1529d
sam#gemmi-win10 <sam@china.gz>
2023-04-08 00:55:04 +0800

    img=kube-cmd:v2.1 -- # TEST_BASH
    _bash
    
    up img_build.sh
    
    kube-cmd:v2.0

--- img_build.sh
+++ img_build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #setenforce 0
+cur=$(cd "$(dirname "$0")"; pwd) 
+cd $cur
 
 #registry
+source /etc/profile; #export |grep DOCKER_REG
 repo=registry.cn-shenzhen.aliyuncs.com
-source /etc/profile #~/.auth-docker-registry #set DOCKER_REGISTRY_PW DOCKER_REGISTRY_USER
-#export |grep DOCKER_REG
 echo "${DOCKER_REGISTRY_PW_sdsir}" |docker login --username=${DOCKER_REGISTRY_USER_sdsir} --password-stdin $repo
 
 ns=infrastlabs
-cur=$(cd "$(dirname "$0")"; pwd) 
+
+# TEST_BASH
+# img=kube-cmd:v2.0-bash #latest #v1.0 
+# docker build --pull -t $repo/$ns/$img -f src/Dockerfile_bash .
+# exit 0
 
 #src
-img=kube-cmd:latest #v1.0 
-docker build --pull -t $repo/$ns/$img  $cur/src/. 
+img=kube-cmd:v2.1 #latest #v1.0 
+docker build --pull -t $repo/$ns/$img -f src/Dockerfile .
 docker push $repo/$ns/$img
