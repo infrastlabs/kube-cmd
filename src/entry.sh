49568c5f0ba52dc70d68d99f45747033e7ba8663
sam#gemmi-win10 <sam@china.gz>
2023-04-25 21:25:18 +0800

    out: runDropbear clusterPodMode

--- src/entry.sh
+++ src/entry.sh
@@ -1,7 +1,45 @@
 #!/usr/bin/env bash
 
 #when $SSHD_ENABLE=true
+function runDropbear(){
+#dropbear
+if [ "$SSHD_ENABLE" = "true" ]; then
+  #dropbear -E -F -R -p 22 -b /etc/motd &
+  dropbear -E -F -R -p 22 &
+fi
+}
 runDropbear
+
+
+function clusterPodMode(){
+#gen-kubeconfig dropbear tiller
+if [ "$SSHD_ENABLE" = "true" ]; then
+  #gen-kubeconfig
+  #export TM_KUBECONFIG_PATH=/opt/gen-kubeconfig
+  #gen-kubeconfig
+  #export KUBECONFIG=$TM_KUBECONFIG_PATH
+  
+  #owner by ctoper: for kkn usage.
+  #chmod 777 /opt #for: error: open /opt/gen-kubeconfig.lock: permission denied
+  #chown ctoper:ctoper $TM_KUBECONFIG_PATH
+
+  #special priviledge, just outside common set.
+  dest=/root/.bashrc
+  #echo "export TM_KUBECONFIG_PATH=/opt/gen-kubeconfig" >> $dest
+  #echo "export KUBECONFIG=\\$TM_KUBECONFIG_PATH" >> $dest
+  dest=/home/ctoper/.bashrc
+  #echo "export TM_KUBECONFIG_PATH=/opt/gen-kubeconfig" >> $dest
+  #echo "export KUBECONFIG=\\$TM_KUBECONFIG_PATH" >> $dest
+
+  #run dropbear
+  runDropbear
+  #dropbear -E -F -R -p 22 -b /etc/motd & ##already done in alpine-ext:weak
+
+
+  ##tiller-local
+  #nohup tiller -listen localhost:44134 > /tmp/log-tiller.log 2>&1 &
+fi
+}
 clusterPodMode
 
 echo "tail -f /dev/null"
