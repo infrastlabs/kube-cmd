dc2c204dd06e8fbe4a52c2ee6e0eb56db63d6fe1
sam#gemmi-win10 <sam@china.gz>
2023-05-12 09:24:39 +0800

    repo ali

--- buildx.sh
+++ buildx.sh
@@ -13,7 +13,7 @@ ver=v2.1 #v2
 case "$1" in
 *)
     repo=registry-1.docker.io
-    # repo="registry.cn-shenzhen.aliyuncs.com" #image-sync推docker:20.10.18,ali仓是支持multiArch的
+    repo="registry.cn-shenzhen.aliyuncs.com" #image-sync推docker:20.10.18,ali仓是支持multiArch的
     img="kube-cmd:$ver"
     # cache
     ali="registry.cn-shenzhen.aliyuncs.com"
